2628ae1ff99073b26f9054bcf43fe6f26097533a
Urie <ufunilas@gmail.com>
2025-01-06 13:59:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template
 import pickle
 import numpy as np
+import os
+
 
 # Initialize the Flask app
 app = Flask(__name__)
@@ -35,3 +37,6 @@ def predict():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    # Bind to the Railway-provided PORT or default to 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
